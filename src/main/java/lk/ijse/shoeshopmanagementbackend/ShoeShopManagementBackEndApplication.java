a6d1b0691a9df952eb02cfb6f4ded5a14ac0da3d
Dilshan-Dekumpitiya <dilshandekumpitiya@gmail.com>
2024-05-05 23:55:51 +0530

    init project

---
+++ src/main/java/lk/ijse/shoeshopmanagementbackend/ShoeShopManagementBackEndApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.shoeshopmanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeShopManagementBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoeShopManagementBackEndApplication.class, args);
+    }
+
+}
