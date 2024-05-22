37238f60cae09e796f3e52215ae8fe9ff443bc21
Dilshan-Dekumpitiya <dilshandekumpitiya@gmail.com>
2024-05-22 23:16:29 +0530

    add variety service

---
+++ src/main/java/lk/ijse/shoeshopmanagementbackend/repository/VarietyServiceDAO.java
@@ -0,0 +1,8 @@
+package lk.ijse.shoeshopmanagementbackend.repository;
+
+import lk.ijse.shoeshopmanagementbackend.entity.VarietyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VarietyServiceDAO extends JpaRepository<VarietyEntity,String> {
+}
+
